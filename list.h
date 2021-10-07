da12c824238e6b64833bb61353bd442459e62b03
diegoAlfonsoRamirez <74227035+diegoAlfonsoRamirez@users.noreply.github.com>
2021-10-07 10:59:57 -0500

    Add files via upload

---
+++ list.h
@@ -0,0 +1,173 @@
+#ifndef DLIST_H
+#define DLIST_H
+#include <string>
+#include <sstream>
+
+template <class T> class DList;
+
+template <class T>
+class DLink{
+private:
+	DLink(T);
+	DLink(T, DLink<T>*, DLink<T>*);
+	DLink(const DLink<T>&);
+	T		value;
+	DLink<T> *previous;
+	DLink<T> *next;
+	
+	friend class DList<T>;
+	
+};
+
+template <class T>
+DLink<T>::DLink(T val) : value(val), next(0), previous(0) {}
+
+template <class T>
+DLink<T>::DLink(T val, DLink *prev, DLink *next){}
+
+template <class T>
+DLink<T>::DLink(const DLink<T> &source){}
+
+template <class T>
+class DList{
+public:
+	DList();
+
+	int search(T);
+	void update(int, T);
+	void iFirst(T);
+	void insertion(T);
+	bool empty() const;
+	std::string toString() const;
+	std::string toString_2() const;
+	std::string toString_3() const;
+	
+private:
+	DLink<T> *head;
+	DLink<T> *tail;
+	int size;
+	
+};
+
+template <class T>
+DList<T>::DList() : head(0), tail(0), size(0) {}
+	
+template <class T>
+int DList<T>::search(T val){
+	DLink<T> *p;
+	p = head;
+	int b = 0;
+	while(((p->value) != val) && (b != (size-1))){
+		p = p->next;
+		b++;
+		
+	}
+	if((p->value) != val){
+		b = -1;
+	}
+	return b;
+	
+}
+
+template <class T>
+void DList<T>::update(int a, T val){
+	DLink<T> *p;
+	if((a <= (size/2)) && (a >= 0)){
+		int b = 0;
+		p = head;
+		while(b != a){
+			p = p->next;
+			b++;
+			
+		};
+		p->value = val;
+		
+	}else if((a > (size/2)) && (a <= (size-1))){
+		int b = size-1;
+		p = tail;
+		while(b != a){
+			p = p->previous;
+			b--;
+			
+		};
+		p->value = val;
+		
+	}else{
+		p->value = -1;
+		
+	}
+	
+}
+
+template <class T>
+void DList<T>::iFirst(T val){
+	DLink<T> *newDLink;
+	newDLink = new DLink<T>(val);
+	tail = newDLink;
+	head = newDLink;
+	size++;
+	
+}
+
+template <class T>
+void DList<T>::insertion(T val){
+	DLink<T> *newDLink, *p;
+	newDLink = new DLink<T>(val);
+	if(empty()){
+		iFirst(val);
+		return;
+		
+	}
+	tail->next = newDLink;
+	newDLink->previous = tail;
+	tail = newDLink;
+	size++;
+	
+}
+	
+template <class T>
+bool DList<T>::empty() const{
+	return (head == 0);
+	
+}
+
+template <class T>
+std::string DList<T>::toString() const {
+	std::stringstream aux;
+	DLink<T> *p;
+	p = head;
+	aux << p->value;
+	if (p->next != 0) {
+		aux << ", ";
+	}
+	p = p->next;
+	return aux.str();
+}
+
+template <class T>
+std::string DList<T>::toString_2() const {
+	std::stringstream aux;
+	DLink<T> *p;
+	p = head;
+	aux << p->value;
+	if (p->next != 0) {
+		aux << ", ";
+	}
+	p = p->next;
+	return aux.str();
+}
+
+template <class T>
+std::string DList<T>::toString_3() const {
+	std::stringstream aux;
+	DLink<T> *p;
+	p = head;
+	aux << p->value;
+	if (p->next != 0) {
+		aux << ", ";
+	}
+	p = p->next;
+	return aux.str();
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "Sorts.h"
+#include "list.h"
 #include "lectorCSV.h"
 #include "lab.h"
 #include <iostream>
@@ -7,41 +8,221 @@ int main(){
 	std::vector<Lab> readFile = csvReader();
 	std::vector<Lab> vector;
 	Sort<Lab> bubbleSort;
-	std::cout << "Bienvenido a la base de datos del laboratorio.\n";
-	std::cout << "Por el momento nuestra base solo admite ordenamiento de los datos.\n";
-	std::cout << "Seleccione como quiere ordenar la base de datos.\n";
-	std::cout << "1. Por rango.\n";
-	std::cout << "2. Por edad.\n";
-	std::cout << "3. Por tiempo trabajado.\n";
-	
+	int aux = 0;
 	int opt = 0;
-	while(opt > 3 || opt < 1){
-		std::cin >> opt;
-		switch(opt){
-			case 1:
-				bubbleSort.rangeBubbleSort(readFile);
-				break;
-				
-			case 2:
-				bubbleSort.ageBubbleSort(readFile);
-				break;
-				
-			case 3:
-				bubbleSort.timeBubbleSort(readFile);
-				break;
+	int a, b = 0;
+	DList<int> e;
+	DList<int> r;
+	DList<int> t;
+	for(int i = 0; i < readFile.size(); i++){
+		e.insertion(readFile[i].getAge());
+		
+	}
+	
+	for(int i = 0; i < readFile.size(); i++){
+		r.insertion(readFile[i].getRange());
+		
+	}
+	
+	for(int i = 0; i < readFile.size(); i++){
+		t.insertion(readFile[i].getTimeY());
+		
+	}
+	
+	std::cout << "Bienvenido a la base de datos del laboratorio.\n";
+	std::cout << "Seleccione que desea hacer.\n";
+	std::cout << "1. Ordenar la base de datos.\n";
+	std::cout << "2. Buscar o actualizar un dato.\n";
+	std::cout << "Pulse cualquier otra tecla para salir.\n";
+	
+	std::cin >> opt;
+	switch(opt){
+		case 1:
+		{
+			aux = 1;
+			std::cout << "Seleccione como quiere ordenar la base de datos.\n";
+			std::cout << "1. Por rango.\n";
+			std::cout << "2. Por edad.\n";
+			std::cout << "3. Por tiempo trabajado.\n";
+			std::cout << "Pulse cualquier otra tecla para salir.\n";
 			
-			default:
-				break;
+			opt = 0;
+			std::cin >> opt;
+			switch(opt){
+				case 1:
+				{
+					bubbleSort.rangeBubbleSort(readFile);
+					break;
+				}
+				
+				case 2:
+				{
+					bubbleSort.ageBubbleSort(readFile);
+					break;
+				}
+					
+				case 3:
+				{
+					bubbleSort.timeBubbleSort(readFile);
+					break;
+				}
+				
+				default:
+					break;
+				
+			}
+			break;
+		}
+		
+		case 2:
+		{
+			aux = 2;
+			std::cout << "Seleccione que tipo de dato desea manejar.\n";
+			std::cout << "1. Rango.\n";
+			std::cout << "2. Edad.\n";
+			std::cout << "3. Tiempo trabajado.\n";
+			std::cout << "Pulse cualquier otra tecla para salir.\n";
+			
+			opt = 0;
+			std::cin >> opt;
+			switch(opt){
+				case 1:
+				{
+					std::cout << "Desea actualizar o buscar datos?.\n";
+					std::cout << "1. Actualizar.\n";
+					std::cout << "2. Buscar.\n";
+					std::cout << "Pulse cualquier otra tecla para salir.\n";
+					a, b = 0;
+					opt = 0;
+					std::cin >> opt;
+					switch(opt){
+						case 1:
+						{
+							std::cout << "Introduzca la posicion del dato a reemplazar.\n";
+							std::cin >> a;
+							std::cout << "Introduzca el valor del dato a reemplazar.\n";
+							std::cin >> b;
+							r.update(a, b);
+							break;
+						}
+						
+						case 2:
+						{
+							std::cout << "Introduzca el valor a encontrar.\n";
+							std::cin >> b;
+							std::cout << r.search(b) << std::endl;
+							break;
+						}
+						
+						default:
+							break;
+							
+					}
+				}
+				
+				case 2:
+				{
+					std::cout << "Desea actualizar o buscar datos?.\n";
+					std::cout << "1. Actualizar.\n";
+					std::cout << "2. Buscar.\n";
+					std::cout << "Pulse cualquier otra tecla para salir.\n";
+					a, b = 0;
+					opt = 0;
+					std::cin >> opt;
+					switch(opt){
+						case 1:
+						{
+							std::cout << "Introduzca la posicion del dato a reemplazar.\n";
+							std::cin >> a;
+							std::cout << "Introduzca el valor del dato a reemplazar.\n";
+							std::cin >> b;
+							e.update(a, b);
+							break;
+						}
+						
+						case 2:
+						{
+							std::cout << "Introduzca el valor a encontrar.\n";
+							std::cin >> b;
+							std::cout << e.search(b) << std::endl;
+							break;
+						}
+						
+						default:
+							break;
+							
+					}
+				}
+				
+				case 3:
+				{
+					std::cout << "Desea actualizar o buscar datos?.\n";
+					std::cout << "1. Actualizar.\n";
+					std::cout << "2. Buscar.\n";
+					std::cout << "Pulse cualquier otra tecla para salir.\n";
+					a, b = 0;
+					opt = 0;
+					std::cin >> opt;
+					switch(opt){
+						case 1:
+						{
+							std::cout << "Introduzca la posicion del dato a reemplazar.\n";
+							std::cin >> a;
+							std::cout << "Introduzca el valor del dato a reemplazar.\n";
+							std::cin >> b;
+							t.update(a, b);
+							break;
+						}
+						
+						case 2:
+						{
+							std::cout << "Introduzca el valor a encontrar.\n";
+							std::cin >> b;
+							std::cout << t.search(b) << std::endl;
+							break;
+						}
+						
+						default:
+							break;
+							
+					}
+												
+				}
+				default:
+					break;
+			}
+			break;
+				
+		}
+			
+		default:
+			break;
+			
+	}
+	
+	if(aux == 1){
+		std::cout << "\n";
+		int k = 1;
+		for(int i = 0; i < readFile.size(); i++){
+			std::cout << " " << k << ". ";
+			std::cout << readFile[i].getName() << " - " << readFile[i].getRange() << " - " << readFile[i].getAge() << " - " << readFile[i].getJob() << " - " << readFile[i].getTimeY() << std::endl;
+			k += 1;
 			
 		}
 		
+	}else if(aux == 2){
+		std::cout << "\n";
+		int k = 1;
+		for(int i = 0; i < readFile.size(); i++){
+			std::cout << " " << k << ". ";
+			std::cout << readFile[i].getName() << " - " << r.toString() << " - " << e.toString_2() << " - " << readFile[i].getJob() << " - " << t.toString_3() << std::endl;
+			k += 1;
+			
+		}
+		
+	}else{
+		aux = 0;
+		
 	}
-	std::cout << "\n";
-	int k = 1;
-	for(int i = 0; i < readFile.size(); i++){
-		std::cout << " " << k << ". ";
-		std::cout << readFile[i].getName() << " - " << readFile[i].getRange() << " - " << readFile[i].getAge() << " - " << readFile[i].getJob() << " - " << readFile[i].getTimeY() << std::endl;
-		k += 1;
-
-	}
+	
 }
